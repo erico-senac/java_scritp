a3690df7142bfbf9a2214a559c74e557461be821
erico-senac <professorerico@pi.senac.br>
2024-04-11 21:55:45 -0300

    mais js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,10 +1,13 @@
 /* seleções */
-const nome = document.querySelector('[nome]');
+const nome = document.querySelector('#nome');
 const curso = document.querySelector('[curso]');
+const js_nome = document.querySelector('[js_nome]');
 
 /* uso das seleções */
 
-nome.innerText = prompt('Qual seu nome?');
-let nome_curso = prompt('Qual seu curso?');
+/* nome.innerText = prompt('Qual seu nome?');
+let nome_curso = prompt('Qual seu curso?'); */
 
-curso.innerHTML = `<div style ="background-color: #456">${nome_curso}</div>`;
+/* curso.innerHTML = `<div style ="background-color: #456">${nome_curso}</div>`; */
+
+js_nome.innerHTML = `<p>Olá eu me chamo <span>${nome.value}</span></p>`
