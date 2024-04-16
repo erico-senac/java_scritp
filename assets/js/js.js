1eda567313009b2b10a54192d4472c642f59b969
erico-senac <professorerico@pi.senac.br>
2024-04-15 21:44:06 -0300

    formulario estilo

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,13 +1,8 @@
 /* seleções */
-const nome = document.querySelector('#nome');
-const curso = document.querySelector('[curso]');
+const nome = document.querySelector('#nome').value;
+const tp_logradouro = document.querySelector('#tp_logradouro').value;
+const logradouro = document.querySelector('#logradouro').value;
+const numero = document.querySelector('#numero_endereco').value;
 const js_nome = document.querySelector('[js_nome]');
 
-/* uso das seleções */
-
-/* nome.innerText = prompt('Qual seu nome?');
-let nome_curso = prompt('Qual seu curso?'); */
-
-/* curso.innerHTML = `<div style ="background-color: #456">${nome_curso}</div>`; */
-
-js_nome.innerHTML = `<p>Olá eu me chamo <span>${nome.value}</span></p>`
+js_nome.innerHTML = `<p>Olá eu me chamo <span>${nome.value}</span></p>`;
